09e8fb61e2e6dbcb525139ab975cbf4d6bfae547
Archangel767 <mjolnir365@gmail.com>
2021-11-11 22:03:20 -0700

    minor formatting changes

--- server.js
+++ server.js
@@ -12,21 +12,18 @@ const app = express()
 mongoose.connect(
   process.env.MONGODB_URL,
   { useUnifiedTopology: true, useNewUrlParser: true },
-  )
-  .then(function(){
+)
+  .then(function () {
     console.log('Connected to DB...')
   })
-  .catch(function(err){
+  .catch(function (err) {
     console.log(err)
   });
 
 
-/*****************/
-/* Define routes */
-/*****************/
 
 
-// List entry route
+// Entry route
 app.get('/gallery', (req, res) => {
   // 3. Define `scene` array of objects using our model
   let scene = null
@@ -38,20 +35,22 @@ app.get('/gallery', (req, res) => {
     else {
       scene = data
       console.log(data)
-    if (typeof scene !== 'undefined' && Array.isArray(scene)) {
+      if (typeof scene !== 'undefined' && Array.isArray(scene)) {
         res.send(scene)
-    } else {    
+      } else {
         res.status(404)
-        res.send({error: 'File Not Found'})      
-    }}
+        res.send({ error: 'File Not Found' })
+      }
+    }
   })
 })
+
 
 // Item route
 app.get('/gallery/:title', (req, res) => {
   let character
-  Scene.findOne({title: req.params.title}, function(err, data) {
-    if(err) {
+  Scene.findOne({ title: req.params.title }, function (err, data) {
+    if (err) {
       console.log(err)
       res.sendStatus(404);
     } else {
@@ -61,27 +60,25 @@ app.get('/gallery/:title', (req, res) => {
         res.send(character)
       } else {
         res.status(404)
-        res.send({error: 'File Not Found'})
+        res.send({ error: 'File Not Found' })
       }
     }
   });
 })
 
-/****************************/
-/* Handle 404, start server */
-/****************************/
+
 
 // Handle 404 errors with middleware
-app.use(function(req, res) {
+app.use(function (req, res) {
 
   // If path starts with `/api`, send JSON 404
   if (req.url.startsWith('/api')) {
 
     res.status(404)
-    res.send({error: 'File Not Found'})
+    res.send({ error: 'File Not Found' })
 
   } else {
-  
+
     // else send HTML 404
     res.status(404)
     res.send('<h1>404: File Not Found</h1>')
@@ -92,6 +89,6 @@ app.use(function(req, res) {
 // Start server
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, function(){
+app.listen(PORT, function () {
   console.log(`Listening on port ${PORT}`);
 });
